e9f907d74de1e9054329731797001bd9578243cd
omibo <omidbodaghi00@gmail.com>
2024-04-09 17:59:36 -0600

    Run health_check test with random port number

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 //! lib.rs
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
 
-async fn health_check() -> impl Responder{
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| { 
         App::new()
-            .route("./health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+            .route("/health_check", web::get().to(health_check))
+        })
+        .listen(listener)?
+        .run();
+
+    Ok(server)
 }
