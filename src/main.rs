474066096ae4f4dc1e97a2dd8710ce400355c92e
Omid Bodaghi <omibo@Omids-MacBook-Pro.local>
2025-04-01 20:30:25 -0700

    Add sql connection script

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
     run(address)?.await
 }
