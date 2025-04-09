55efc479ad6946d5e37fdb05f47c745f67b63285
Omid Bodaghi <omibo@Omids-MacBook-Pro.local>
2025-04-08 21:39:23 -0700

    Isolate tests

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,25 @@
-use serde::Deserialize;
-
-#[derive(Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct Settings {
-    pub database: DatabseSettings,
+    pub database: DatabaseSettings,
     pub application_port: u16,
 }
 
-#[derive(Deserialize)]
-pub struct DatabseSettings {
+#[derive(serde::Deserialize, Clone)]
+pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
@@ -23,13 +30,4 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         ))
         .build()?;
     settings.try_deserialize::<Settings>()
-}
-
-impl DatabseSettings {
-    pub fn connection_string(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
-        )
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -5,6 +6,9 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
-    run(address)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgress");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
+    run(listener, connection_pool)?.await
 }
